5b63bf6744dccc4dc88bf6981f20d9480be99077
wujianguo <github@wujianguo.org>
2015-12-28 21:28:06 +0800

    init

---
+++ YouSirCmd/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  YouSirCmd
+//
+//  Created by wujianguo on 15/12/28.
+//  Copyright © 2015年 wujianguo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
